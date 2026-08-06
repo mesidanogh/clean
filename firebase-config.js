d8d129476874f05f8a76918ced211b164ececa4d
mesida <mesida@mesidanoMacBook-Pro.local>
2026-08-06 13:14:46 +0900

    Firebaseプロジェクトの接続情報を設定

--- firebase-config.js
+++ firebase-config.js
@@ -4,10 +4,10 @@
 // 詳しい手順は README.md を参照してください。
 
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBghcS7dgYcEK59tFm0uJsDg4_FIdL0NfM",
+  authDomain: "clen-46a2b.firebaseapp.com",
+  projectId: "clen-46a2b",
+  storageBucket: "clen-46a2b.firebasestorage.app",
+  messagingSenderId: "98009711727",
+  appId: "1:98009711727:web:73d10fe047285b3df0b99a",
 };
